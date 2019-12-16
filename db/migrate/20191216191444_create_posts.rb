51a207ce0d4e4fa8bf38a2eccb32eb46ea82ead7
alekskojem <alekskojem@gmail.com>
2019-12-16 21:16:19 +0200

    Add model Post and run migrate

--- db/migrate/20191216191444_create_posts.rb
+++ db/migrate/20191216191444_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.belong_to :user
+      t.belongs_to :user, foreign_key: true
       t.string :title
       t.text :body
 
