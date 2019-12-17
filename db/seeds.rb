7a2d6b786f5791b66a356fb381dc990b437078f2
alekskojem <alekskojem@gmail.com>
2019-12-17 17:50:13 +0200

    Add gem Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+	user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+	5.times do
+		user.post.create(title: Faker::Lorem.sentence(world_count:3), body: Faker::Lorem::paragraph(sentence_count:3))
+	end
+
+	
+end
